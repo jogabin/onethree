10c1b4c835ad801fde9ccc9c5f8e8b59244719ad
jdagil3 <jdagil3@gmail.com>
2019-05-28 17:19:31 +0900

    로그관련 업데이트

--- src/main/java/com/onethree/home/index/IndexController.java
+++ src/main/java/com/onethree/home/index/IndexController.java
@@ -1,20 +1,11 @@
 package com.onethree.home.index;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.text.SimpleDateFormat;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +14,9 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
+	
+	private static final Logger LOGGER = LogManager.getLogger(IndexController.class.getName());
+
 	
 	//메뉴 타이틀
 	String contentTitle = "onethree";
@@ -44,6 +38,8 @@ public class IndexController {
 		
 		//2번째 파라미터 서브타이틀
 		setMenuTitle(model,"Main");
+		
+		LOGGER.info("========================== MAIN INFO =======================================");
 		
 		return "main/main";
 	}
